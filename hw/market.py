a2e039910fca9872f8745a1f6325adeef48822e4
ChillyApril <sashaokho@gmail.com>
2024-01-15 19:34:28 +0500

    All tasks are done.

--- hw/market.py
+++ hw/market.py
@@ -1,28 +1,20 @@
+from __init__ import time_decorator, time_decorator_with_return
 class Market:
+    @time_decorator
     def __init__(self, wines: list = None, beers: list = None) -> None:
-        pass
-
+        self.wines = wines
+        if wines == None:
+            self.wines = []
+        self.beers = beers
+        if beers == None:
+            self.beers = []
+        self.titles = set([wine.title for wine in self.wines]) | set([beer.title for beer in self.beers])
+    @time_decorator_with_return
     def has_drink_with_title(self, title=None) -> bool:
-        """
-        Проверяет наличие напитка в магазине за О(1)
-
-        :param title:
-        :return: True|False
-        """
-        pass
-
+        return (title in self.titles)
+    @time_decorator_with_return
     def get_drinks_sorted_by_title(self) -> list:
-        """
-        Метод получения списка напитков (вина и пива) отсортированных по title
-
-        :return: list
-        """
-        pass
-
+        return list(sorted(self.wines + self.beers, key = (lambda x: x.title)))
+    @time_decorator_with_return
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
-        """
-        Метод получения списка напитков в указанном диапазоне дат: с from_date по to_date
-
-        :return: list
-        """
-        pass
+        return [drink for drink in (self.wines + self.beers) if from_date < drink.production_date < to_date]
